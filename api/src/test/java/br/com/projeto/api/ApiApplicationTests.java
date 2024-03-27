571aded135958386a14c7c4c2d363316b6c8e10c
[T]/Marcus_Vini\[T]/ <marcussouzatrab@gmail.com>
2024-03-27 12:23:46 -0300

    para o curriculo

---
+++ api/src/test/java/br/com/projeto/api/ApiApplicationTests.java
@@ -0,0 +1,14 @@
+package br.com.projeto.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+class ApiApplicationTests {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiApplication.class, args);
+	}
+
+}
